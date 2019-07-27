98be5753afee9a92ced468370a61743a4f46c5c3
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-27 03:13:32 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -24,54 +24,54 @@ HELP
   puts help
 end
 
-help 
+help
 
-def list(songs) 
+def list(songs)
   songs.each_with_index { |item, index|
     puts "#{index+1}. #{item}" }
-end 
+end
 
 list(songs)
 
 def play(songs)
   puts "Please enter a song name or number:"
-  user_response = gets.downcase.chomp 
-  
+  user_response = gets.downcase.chomp
+
   if (1..9).to_a.include?(user_response.to_i)
     puts "Playing #{songs[user_response.to_i - 1]}"
     elsif songs.include?(user_response)
     puts "Playing #{user_response}"
-  else 
+  else
     puts "Invalid input, please try again"
-  end 
-end 
+  end
+end
 
 play(songs)
 
 def exit_jukebox
   puts "Goodbye"
-end 
+end
 
 def run(songs)
   #help
-  command = "" 
-  while command  
+  command = ""
+  while command
   puts "Please enter a command:"
-  command = gets.downcase.strip  
-  case command 
+  command = gets.downcase.strip
+  case command
     when 'list'
       list(songs)
       when 'play'
         list(songs)
         play(songs)
       when 'help'
-        help 
+        help
       when 'exit'
         exit_jukebox
-        break 
-      else 
-        help 
-      end 
-    end 
-  end 
+        break
+      else
+        help
+      end
+    end
+  end
   run(songs)
